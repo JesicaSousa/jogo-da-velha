79614881b94d5dc31b394bff4bf931c611d7b1fc
Jesica Sousa <89730976+JesicaSousa@users.noreply.github.com>
2023-03-18 17:47:02 -0300

    second commit

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-// define o jogador atual como "X"
-let currentPlayer = "X";
-// cria um objeto para armazenar o placar do usuário
-let score = {
-  userScore: 0
-};
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
-// seleciona todos os quadrados do jogo da velha
-const squares = document.querySelectorAll(".square");
-// seleciona o elemento de score do usuário
-const userScore = document.querySelector("#userScore");
+const cells = document.querySelectorAll('.cell');
+const message = document.getElementById('message');
+const resetButton = document.getElementById('reset');
 
-// adiciona um listener de click a todos os quadrados
-squares.forEach(square => {
-  square.addEventListener("click", () => {
-    // se o quadrado já estiver marcado, sai da função
-    if (square.textContent !== "") {
-      return;
-    }
-    // marca o quadrado com o jogador atual
-    square.textContent = currentPlayer;
-    // alterna o jogador atual
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    // verifica se alguém ganhou
-    if (checkWin()) {
-      // se alguém ganhou, adiciona um ponto ao score do usuário
-      score.userScore++;
-      // atualiza o score na página
-      userScore.textContent = score.userScore;
-      // reinicia o jogo
-      reset();
-    } else if (checkTie()) {
-      // se o jogo empatou, reinicia o jogo
-      reset();
-    }
+let currentClass;
+let board = ['', '', '', '', '', '', '', '', ''];
+let isGameOver = false;
+
+startGame();
+
+resetButton.addEventListener('click', startGame);
+
+function startGame() {
+  board = ['', '', '', '', '', '', '', '', ''];
+  isGameOver = false;
+  currentClass = X_CLASS;
+  cells.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
   });
-});
+  message.innerText = '';
+}
 
-// verifica se alguém ganhou
+function handleClick(e) {
+  const cell = e.target;
+  const cellIndex = Array.from(cells).indexOf(cell);
+  if (board[cellIndex] !== '' || isGameOver) return;
+  board[cellIndex] = currentClass;
+  cell.classList.add(currentClass);
+  if (checkWin()) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+  }
+}
+
+function swapTurns() {
+  currentClass = currentClass === X_CLASS ? O_CLASS : X_CLASS;
+}
+
 function checkWin() {
-  const rows = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < rows.length; i++) {
-    const [a, b, c] = rows[i];
-    if (squares[a].textContent !== "" && squares[a].textContent === squares[b].textContent && squares[a].textContent === squares[c].textContent) {
-        
-    }
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return board[index] === currentClass;
+    });
+  });
+}
+
+function isDraw() { }
+ 
