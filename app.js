3e49c63273ce0688a5baff4f83232d7433033633
Jesica Sousa <Jesicasousa1997@gmail.com>
2023-03-29 22:23:25 -0300

    versão final

--- app.js
+++ app.js
@@ -1,75 +1,61 @@
-let board = [  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+let currentPlayer = 'X';
+let gameboard = ['', '', '', '', '', '', '', '', ''];
 
-let player = 'X';
-let gameOver = false;
-
-function displayBoard() {
-  console.log(board[0][0] + '|' + board[0][1] + '|' + board[0][2]);
-  console.log('-+-+-');
-  console.log(board[1][0] + '|' + board[1][1] + '|' + board[1][2]);
-  console.log('-+-+-');
-  console.log(board[2][0] + '|' + board[2][1] + '|' + board[2][2]);
+function render() {
+  gameboard.forEach((value, index) => {
+    document.getElementById(index).innerText = value;
+  });
 }
 
-function checkWin() {
-  for (let i = 0; i < 3; i++) {
-    if (board[i][0] !== ' ' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-      return true;
+function handlePlayerClick(event) {
+  const cellIndex = event.target.id;
+  if (gameboard[cellIndex] === '') {
+    gameboard[cellIndex] = currentPlayer;
+    render();
+    checkWinner();
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  }
+}
+
+function handleResetClick() {
+  currentPlayer = 'X';
+  gameboard = ['', '', '', '', '', '', '', '', ''];
+  render();
+}
+
+function checkWinner() {
+  const winningCombos = [    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [a, b, c] = winningCombos[i];
+    if (gameboard[a] && gameboard[a] === gameboard[b] && gameboard[a] === gameboard[c]) {
+      alert(`Parabéns! ${currentPlayer} venceu o jogo!`);
+      handleResetClick();
+      return;
     }
   }
 
-  for (let j = 0; j < 3; j++) {
-    if (board[0][j] !== ' ' && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
-      return true;
-    }
-  }
-
-  if (board[0][0] !== ' ' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-    return true;
-  }
-
-  if (board[0][2] !== ' ' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-    return true;
-  }
-
-  return false;
-}
-
-function switchPlayer() {
-  player = player === 'X' ? 'O' : 'X';
-}
-
-while (!gameOver) {
-  console.clear();
-  displayBoard();
-  console.log(`É a vez do jogador ${player}.`)
-
-  let row1 = prompt('Digite a linha (1, 2 ou 3)');
-  let row = parseInt(row1)
-  let col1 = prompt('Digite a coluna (1, 2 ou 3)');
-  let col = parseInt(col1)
-
-  if (isNaN(row) || isNaN(col) || row < 1 || row > 3 || col < 1 || col > 3 || board[row - 1][col - 1] !== ' ') {
-    console.log('Jogada inválida. Tente novamente.');
-    continue;
-  }
-
-  board[row - 1][col - 1] = player; 
-
-  if (checkWin()) {
-    console.clear();
-    displayBoard();
-    console.log(`O jogador ${player} venceu!`);
-    gameOver = true;
-  } else if (!board.flat().includes(' ')) {
-    console.clear();
-    displayBoard();
-    console.log('Empate!');
-    gameOver = true;
-  } else {
-    switchPlayer();
+  if (!gameboard.includes('')) {
+    alert('Empate!');
+    handleResetClick();
   }
 }
+
+render();
+
+document.querySelectorAll('button').forEach((button) => {
+  button.addEventListener('click', handlePlayerClick);
+});
+
+document.getElementById('reset').addEventListener('click', handleResetClick);
+
+
+
